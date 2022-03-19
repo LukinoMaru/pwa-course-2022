9dd16840bbaeb96a21a7ebc7180a56b906432a10
LukinoNeko <varintorn.athi@bumail.net>
2022-03-19 16:05:48 +0700

    Have a cat fact here

--- src/components/Chatline.js
+++ src/components/Chatline.js
@@ -14,22 +14,40 @@ function Chatline(props) {
       const html = text.split(" ").slice(1).join(" ");
       setElement(<div dangerouslySetInnerHTML={{ __html: html }}></div>);
     }
+
     else if (text.startsWith("/age")) {
       const [, name] = text.split(" ");
+
       fetch("https://api.agify.io/?name=" + name)
         .then(res => res.json())
-        .then(data => setElement(data.age));
+        .then(data => setElement(data.name + data.age || "ไม่รู้วว"));
     }
+
     else if (text.startsWith("/gender")) {
       const [, name] = text.split(" ");
       fetch("https://api.genderize.io/?name=" + name)
         .then(res => res.json())
-        .then(data => setElement(data.gender));
+        .then(data => setElement(data.gender  || "ไม่เจอ"));
     }
+
     else if (text.startsWith("/randomfact")) {
       // ex10-01 here: Implement randomfact using fetch & promise
       // https://uselessfacts.jsph.pl/random.json?language=en
 
+    }
+
+    else if (text.startsWith("/nationalize")) {
+      const [, name] = text.split(" ");
+      fetch("https://api.nationalize.io/?name=" + name)
+        .then(res => res.json())
+        //.then(data => setElement(data.country || "ไม่มีย์"));
+    }
+
+    else if (text.startsWith("/randomcat")) {
+      const [, name] = text.split(" ");
+      fetch("https://catfact.ninja/fact")
+        .then(res => res.json())
+        .then(data => setElement(data.fact ||"ไม่มีแมว"));
     }
   };
 
